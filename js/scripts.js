b4c5cb6c15bd4e603560d0a9ee974520390c3d93
mscherzer <96539233+mscherzer@users.noreply.github.com>
2022-01-19 16:16:04 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbx6i70qO36o9I5_-b1KiYhwAWkgx7Wo_rFLQrpEXVdwBHOFRqgFUjeHsCJcZfj6Dad7Jw/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
